32aa78bf3246600a02815ab866d37b80e134fc7d
kmsheng <kmsh3ng@gmail.com>
2018-12-12 14:12:57 +0800

    update solution for 7kyu Highest and lowest

--- 7kyu-Highest-and-lowest.go
+++ 7kyu-Highest-and-lowest.go
@@ -6,14 +6,12 @@ import "sort"
 import "strconv"
 
 func HighAndLow(str string) string {
-	strs := strings.Fields(str)
-	nums := []int{}
-	for _, s := range strs {
-		parsed, _ := strconv.Atoi(s)
-		nums = append(nums, parsed)
-	}
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] > nums[j]
-	})
-	return fmt.Sprintf("%d %d", nums[0], nums[len(nums) - 1])
+  strs := strings.Fields(str)
+  nums := []int{}
+  for _, s := range strs {
+    parsed, _ := strconv.Atoi(s)
+    nums = append(nums, parsed)
+  }
+  sort.Ints(nums)
+  return fmt.Sprintf("%d %d", nums[len(nums) - 1], nums[0])
 }
