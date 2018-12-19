ae15bc3e11f35776c4a0ade6874a12ce7fa50e29
kmsheng <kmsh3ng@gmail.com>
2018-12-19 15:24:10 +0800

    update solution for 7kyu Are the numbers in order?

--- 7kyu-Are-the-numbers-in-order.go
+++ 7kyu-Are-the-numbers-in-order.go
@@ -3,13 +3,5 @@ package kata
 import "sort"
 
 func InAscOrder(numbers []int) bool {
-	arr := make([]int, len(numbers))
-	copy(arr, numbers)
-	sort.Ints(arr)
-	for i, _ := range numbers {
-		if arr[i] != numbers[i] {
-			return false
-		}
-	}
-	return true
+	return sort.IntsAreSorted(numbers)
 }
